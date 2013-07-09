d1b595df68a3f2b3d5cc7beb919f8a2371837fab
Catalin Balan <laserbeam333@gmail.com>
2013-07-09 14:15:52 +0300

    hello from index

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var filename = 'index.html'
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var text = new Buffer(fs.readFileSync(filename), 'utf-8')
+  response.send(text.toString());
 });
 
 var port = process.env.PORT || 5000;
