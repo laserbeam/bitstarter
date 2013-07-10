2d6614a6ef6d09f4ff6cb6cdc9e7be3c151d5d45
Catalin Balan <laserbeam333@gmail.com>
2013-07-10 18:11:35 +0300

    grader made, fixed typo in index

--- grader.js
+++ grader.js
@@ -23,6 +23,7 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
+var rest = require('restler')
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = 'checks.json';
@@ -36,16 +37,16 @@ var assertFileExists = function(infile) {
 	return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-	return cheerio.load(fs.readFileSync(htmlfile));
-};
+// var cheerioHtmlFile = function(htmlfile) {
+// 	return cheerio.load(fs.readFileSync(htmlfile));
+// };
 
 var loadChecks = function(checksfile) {
 	return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-	$ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlstring, checksfile) {
+	$ = cheerio.load(htmlstring);
 	var checks = loadChecks(checksfile).sort();
 	var out = {};
 	for(var i in checks) {
@@ -53,25 +54,44 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 		out[checks[i]] = present;
 	}
 	return out;
-}
+};
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
-	return fn.bind({})
-}
+	return fn.bind({});
+};
+
+var outputResult = function(checkJson) {
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+};
 
 if(require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <html_file>', 'URL to file')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	var htmlstring;
+	var checkJson;
+	if(program.url) {
+		htmlstring = rest.get(program.url).on('complete', function(result, response){
+			if (result instanceof Error) {
+	            console.error('Error: ' + util.format(response.message));
+	        } else {
+	        	checkJson = checkHtmlFile(result, program.checks);
+	            outputResult(checkJson);
+	        }
+		});
+	} else {
+		htmlstring = fs.readFileSync(program.file);
+		checkJson = checkHtmlFile(htmlstring, program.checks);
+		outputResult(checkJson);	
+	}
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
-}
+};
 
 
 
